8e175f92326432b8a95f6f9fd2ec11d43b94fc6a
jakobsaadbye <31799001+jakobsaadbye@users.noreply.github.com>
2024-10-07 13:57:35 +0200

    Fix Makefile

--- build/out.asm
+++ build/out.asm
@@ -9,12 +9,11 @@ segment .data
    string_false DB "false", 0
    string_true  DB "true", 0
    string_assert_fail  DB "Assertion failed at line %d", 10, 0
-   __DogKind.samoid DB "DogKind.samoid", 0
-   __DogKind.chiwawa DB "DogKind.chiwawa", 0
-   __DogKind.bulldog DB "DogKind.bulldog", 0
-   CF0 DD 20.0000000
-   CF1 DD 0.5000000
-   CF2 DD 80.0000000
+   CF0 DD 1234.1234568
+   CF1 DQ 1234.123456789000102
+   CS2 DB `%f`, 10, 0 
+   CS3 DB `%f`, 10, 0 
+   CS4 DB `%lf`, 10, 0 
 
 segment .text
    global main
@@ -33,119 +32,63 @@ assert_fail:
    mov		rcx, 1
    call		ExitProcess
 
-print_enum_DogKind:
-   mov		r8, 0
-   cmp		rdx, r8
-   jz			enum_case_0
-   mov		r8, 1
-   cmp		rdx, r8
-   jz			enum_case_1
-   mov		r8, 2
-   cmp		rdx, r8
-   jz			enum_case_2
-   push		rcx
-   mov		r8, rdx
-   mov		rdx, fmt_int
-   push		r8
-   push		rdx
-   push		rcx
-   call		sprintf
-   pop		rax
-   pop		rbx
-   pop		rbx
-   pop		rbx
-   ret
-enum_case_0:
-   mov		rax, __DogKind.samoid
-   ret
-enum_case_1:
-   mov		rax, __DogKind.chiwawa
-   ret
-enum_case_2:
-   mov		rax, __DogKind.bulldog
-   ret
 
 main:
    push		rbp
    mov		rbp, rsp
    sub		rsp, 64
 
-   ; initialization of 'samoid'
-   mov		DWORD -16[rbp], 0
-   mov		DWORD -12[rbp], 0
-   mov		DWORD -8[rbp], 0
+   ; initialization of 'x'
    mov		DWORD -4[rbp], 0
-   push		0
-   pop		rax
-   mov		DWORD -16[rbp], eax
-   mov		rax, 4
+   mov		rax, 0
    push		rax
    pop		rax
-   mov		DWORD -12[rbp], eax
+   cvtsi2ss	xmm0, rax
+   movd		-4[rbp], xmm0
+
+   ; initialization of 'y'
+   mov		DWORD -8[rbp], 0
    movss		xmm0, [CF0]
    movd		eax, xmm0
    push		rax
    pop		rax
    mov		-8[rbp], eax
-   movss		xmm0, [CF1]
-   movd		eax, xmm0
+
+   ; initialization of 'z'
+   mov		DWORD -16[rbp], 0
+   movsd		xmm0, [CF1]
+   movq		rax, xmm0
    push		rax
    pop		rax
-   mov		-4[rbp], eax
+   mov		-16[rbp], rax
 
-   ; initialization of 'bmi'
-   mov		DWORD -20[rbp], 0
+   ; expression of print
+   mov		eax, -4[rbp]
+   push		rax
+   pop		rdx
+   movq		xmm0, rdx
+   cvtss2sd	xmm0, xmm0
+   movq		rdx, xmm0
+   mov		rcx, CS2
+   call		printf
+
+   ; expression of print
    mov		eax, -8[rbp]
    push		rax
-   mov		eax, -4[rbp]
+   pop		rdx
+   movq		xmm0, rdx
+   cvtss2sd	xmm0, xmm0
+   movq		rdx, xmm0
+   mov		rcx, CS3
+   call		printf
+
+   ; expression of print
+   mov		rax, -16[rbp]
    push		rax
-   mov		eax, -4[rbp]
-   push		rax
-   pop		rbx
-   pop		rax
-   movd	xmm1, ebx
-   movd	xmm0, eax
-   mulss		xmm0, xmm1
-   movd		eax, xmm0
-   push		rax
-   pop		rbx
-   pop		rax
-   movd	xmm1, ebx
-   movd	xmm0, eax
-   divss		xmm0, xmm1
-   movd		eax, xmm0
-   push		rax
-   pop		rax
-   mov		-20[rbp], eax
-   mov		eax, -20[rbp]
-   push		rax
-   movss		xmm0, [CF2]
-   movd		eax, xmm0
-   push		rax
-   pop		rbx
-   pop		rax
-   movd	xmm1, ebx
-   movd	xmm0, eax
-   comiss	xmm0, xmm1
-   sete		al
-   push		rax
-   pop		rcx
-   mov		rdx, 23
-   call		assert
-   mov		eax, DWORD -12[rbp]
-   movsx		rax, eax
-   push		rax
-   mov		rax, 4
-   push		rax
-   pop		rbx
-   pop		rax
-   cmp		rax, rbx
-   sete		al
-   push		rax
-   pop		rcx
-   mov		rdx, 24
-   call		assert
-L4:
+   pop		rdx
+   mov		rcx, CS4
+   call		printf
+L0:
    mov		rax, 0
    add		rsp, 64
    pop		rbp
---
+++ src/llvm_converter.c
@@ -0,0 +1,25 @@
+#include "parser.c"
+
+typedef struct LLVMConverter {
+    Parser *parser;
+} LLVMConverter;
+
+void llvm_emit_binary(AstBinary *bin) {
+    return;
+}
+
+void llvm_emit_expression(AstExpr *expr) {
+    switch (expr->head.type) {
+    case AST_BINARY: bc_emit_binary((AstBinary *)expr); return;
+    default:
+        printf("Internal Compiler Error: Unknown case %s in llvm_emit_expression()", ast_to_str((Ast *)expr));
+    }
+}
+
+void llvm_emit_statement(Ast *ast) {
+    switch (ast->type) {
+    case AST_EXPR: bc_emit_expression((AstExpr *)ast); return;
+    default:
+        printf("Internal Compiler Error: Unknown case %s in llvm_emit_statement()", ast_to_str(ast));
+    }
+}
